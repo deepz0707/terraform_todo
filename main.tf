f61db1105fa58c8d96c02e70adf2a76224fb28fc
deepz0707 <deepak.singh007@hotmail.com>
2025-07-05 10:35:11 +0530

    new

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "ExampleRG" {
     name = "RG1"
     location = "East US"
 }
+resource "azurerm_storage_account" "ExampleStorageAccount" {
+    name                     = "examplestorageacct"
+    resource_group_name      = azurerm_resource_group.ExampleRG.name
+    location                 = azurerm_resource_group.ExampleRG.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
